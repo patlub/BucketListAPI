b24ea943c0212dc5b108020d79ea6ef2cb4eadc9
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-23 13:15:02 +0300

    Create project structure and add environment configuration

---
+++ instance/config.py
@@ -0,0 +1,33 @@
+"""
+Configuration settings for different app environments
+"""
+import os
+
+
+class MainConfiguration(object):
+    DEBUG = False
+    WTF_CSRF_ENABLED = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.urandom(24)
+
+class TestingEnvironment(MainConfiguration):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://@localhost/testing_db'
+
+class DevelopmentEnvironment(MainConfiguration):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://@localhost/bucketList_db'
+
+class ProductionEnvironment(MainConfiguration):
+    DEBUG = False
+    TESTING = False
+
+# Dictionary of different configuration environments
+application_config = {
+    'MainConfig' : MainConfiguration,
+    'TestingEnv' : TestingEnvironment,
+    'DevelopmentEnv' : DevelopmentEnvironment,
+    'ProductionEnv' : ProductionEnvironment
+}
