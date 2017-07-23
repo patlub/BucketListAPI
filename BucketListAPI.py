fb24114b4fc29d90dcc722af5e488874eaa2d415
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-23 16:43:40 +0300

    Set evironment in main

--- BucketListAPI.py
+++ BucketListAPI.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from instance.config import application_config
 
 app = Flask(__name__)
 
+def Env_name(env):
+    app.config.from_object(application_config[env])
+
+Env_name('developmentEnv')
+databases = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
