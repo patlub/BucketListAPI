52d4b8c4d23463224ca658cc1d435b32703626e4
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-30 20:06:26 +0300

    Import modals to manage.py

---
+++ manage.py
@@ -0,0 +1,13 @@
+from flask_script import Manager  # class for handling a set of commands
+from flask_migrate import Migrate, MigrateCommand
+from api import db, create_app
+from modals import modals
+
+app = create_app(config_name='DevelopmentEnv')
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
