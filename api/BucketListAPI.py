6eb62800c2843eb8bb9a9c1e36a01ae7a03d2e83
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-31 13:48:52 +0300

    Implement index route

--- api/BucketListAPI.py
+++ api/BucketListAPI.py
@@ -1,21 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from modals.modals import User, Bucket, Item
-from api.__init__ import create_app, db
+from api import create_app, db
 
 app = create_app('DevelopmentEnv')
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    response = jsonify({'error': 'The request can not be completed'})
-    response.status_code = 404
+@app.route('/')
+def index():
+    response = jsonify({'Welcome Message': 'Hello'})
+    response.status_code = 201
     return response
 
-@app.errorhandler(401)
-def invalid_token(e):
-    response = jsonify({'error': 'Invalid Token'})
-    response.status_code = 401
-    return response
 
 
 if __name__ == '__main__':
