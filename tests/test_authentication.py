c04b9813b5d6d3f8bc8eaa7be2d49d32f150aaf2
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-31 13:50:00 +0300

    Add test for user with missing credentials

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -20,6 +20,17 @@ class AuthenticationTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIn('Welcome Message', response.data.decode())
 
+    def test_registration_with_missing_dredentials(self):
+        """Should throw error for missing credentials"""
+        user = json.dumps({
+            'name': '',
+            'email': '',
+            'password': ''
+        })
+        response = self.client.post('/auth/register', data=user)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Missing', response.data.decode())
+
     def tearDown(self):
         # Drop all tables
         with app.app_context():
